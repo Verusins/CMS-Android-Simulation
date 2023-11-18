c2e97484ac785c4f878d24d43fc9058d510f17ff
Nitanon Rachapradit <98104447+Blue-Peanuts@users.noreply.github.com>
2023-11-17 19:56:11 -0500

    fixed errors from merging dashboard

--- app/src/main/java/example/com/cmsandroidsimulation/AnnouncementFragment.java
+++ app/src/main/java/example/com/cmsandroidsimulation/AnnouncementFragment.java
@@ -5,6 +5,8 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import example.com.cmsandroidsimulation.models.Announcement;
+
 public class AnnouncementFragment extends AppCompatActivity {
 
     @Override
@@ -13,9 +15,8 @@ public class AnnouncementFragment extends AppCompatActivity {
         setContentView(R.layout.fragment_dashboard_student);
 
         // Assuming you have an Announcement object
-        Announcement announcement = new Announcement();
-        announcement.setTitle("Your Announcement Title");
-        announcement.setDescription("This is the content of your announcement.");
+        Announcement announcement = new Announcement("",
+                "Your Announcement Title", "This is the content of your announcement.");
 
         // Find the TextView in your layout by its ID
         TextView announcementTitleTextView = findViewById(R.id.announcementTitle);
@@ -23,6 +24,6 @@ public class AnnouncementFragment extends AppCompatActivity {
 
         // Set the text of the TextViews based on the Announcement object
         announcementTitleTextView.setText(announcement.getTitle());
-        announcementContentTextView.setText(announcement.getDescription());
+        announcementContentTextView.setText(announcement.getDetails());
     }
 }
