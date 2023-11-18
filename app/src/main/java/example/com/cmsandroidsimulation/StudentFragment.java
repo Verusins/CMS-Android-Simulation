6a4fc2ace1d13e34be45a910a90c22b546084fad
Verusins <115052147+Verusins@users.noreply.github.com>
2023-11-18 09:58:58 -0500

    Dashboard Navbar Finished
    Added the sidebar icons, missed a few icons in the figma design but it should work for now.

--- app/src/main/java/example/com/cmsandroidsimulation/StudentFragment.java
+++ app/src/main/java/example/com/cmsandroidsimulation/StudentFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import example.com.cmsandroidsimulation.databinding.FragmentStudentBinding;
@@ -19,5 +20,13 @@ public final class StudentFragment extends Fragment {
 
         binding = FragmentStudentBinding.inflate(inflater, container, false);
         return binding.getRoot();
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+//        binding.dashboardStudent.menuIcon.setOnClickListener(
+//
+//        );
     }
 }
