f3039c566dfcec808d23a689bc9a772d9bd3aadf
eveev26 <89487054+eveev26@users.noreply.github.com>
2023-11-17 19:35:04 -0500

    testing views

--- app/src/main/java/example/com/cmsandroidsimulation/PostReqFragment.java
+++ app/src/main/java/example/com/cmsandroidsimulation/PostReqFragment.java
@@ -39,10 +39,23 @@ public class PostReqFragment extends Fragment {
             public void onClick(View view) {
                 req.setTargetPost(getString(R.string.post_quiz_q1_cs_specialist));
                 binding.postQuizQ2.setVisibility(View.VISIBLE);
-                Log.i("", req.getTargetPost());
             }
         });
 
+        view.findViewById(R.id.post_quiz_q1_cs_major).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                req.setTargetPost(getString(R.string.post_quiz_q1_cs_major));
+                binding.postQuizQ2.setVisibility(View.VISIBLE);
+            }
+        });
+
+        view.findViewById(R.id.post_quiz_q1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.postQuizQ2.setVisibility(View.VISIBLE);
+            }
+        });
 
     }
 }
