0963281f20b6a831ce04686021b0cd27a7e44cde
Verusins <115052147+Verusins@users.noreply.github.com>
2023-11-20 09:45:16 -0500

    Display Placeholder values on dashboard
    They look really messy but yeah they works, but not in the right place.

--- app/src/main/java/example/com/cmsandroidsimulation/DashboardStudentFragment.java
+++ app/src/main/java/example/com/cmsandroidsimulation/DashboardStudentFragment.java
@@ -2,16 +2,27 @@ package example.com.cmsandroidsimulation;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.events.Event;
+
+import java.util.ArrayList;
+
 import example.com.cmsandroidsimulation.databinding.FragmentDashboardStudentBinding;
+import example.com.cmsandroidsimulation.models.Announcement;
+import example.com.cmsandroidsimulation.models.EventInfo;
+import example.com.cmsandroidsimulation.models.PlaceholderValues;
 
 public class DashboardStudentFragment extends Fragment {
     FragmentDashboardStudentBinding binding;
@@ -32,6 +43,57 @@ public class DashboardStudentFragment extends Fragment {
 
 //        Disable Sidebar on create
         sidebar.setVisibility(View.GONE);
+
+//        List Announcements from database
+        final RelativeLayout announcementParentWrapper = binding.announcements;
+        ArrayList<Announcement> announcementsSource = PlaceholderValues.generateTestAnnouncementList();
+        int index = 0;
+        for(Announcement announcement: announcementsSource) {
+            View childView = getLayoutInflater().inflate(R.layout.announcement_item, null);
+            String title = announcement.getTitle(), content = announcement.getDetails();
+//            LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) childView.getLayoutParams();
+
+            TextView titleTextView = childView.findViewById(R.id.announcement_title_text);
+            TextView contentTextView = childView.findViewById(R.id.announcement_content_text);
+
+            titleTextView.setText(title);
+            contentTextView.setText(content);
+
+//            layoutParams.topMargin = 100*index + 20;
+
+            childView.findViewById(R.id.close_announcement).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    childView.setVisibility(View.GONE);
+                }
+            });
+
+            announcementParentWrapper.addView(childView);
+            index ++;
+        }
+
+//        Event from db
+        //        List Announcements from database
+        final RelativeLayout eventParentWrapper = binding.events;
+        ArrayList<EventInfo> eventsSource = PlaceholderValues.generateTestEventInfoList();
+        for(EventInfo event: eventsSource) {
+            View childView = getLayoutInflater().inflate(R.layout.event_item, null);
+            String title = event.getTitle(), content = event.getDetails();
+//            LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) childView.getLayoutParams();
+
+            TextView titleTextView = childView.findViewById(R.id.event_title);
+            TextView contentTextView = childView.findViewById(R.id.event_content);
+
+            titleTextView.setText(title);
+            contentTextView.setText(content);
+
+//            layoutParams.topMargin = 100*index2 + 20;
+
+            announcementParentWrapper.addView(childView);
+        }
+
+
+
 
 //        Open Sidebar
         binding.menuIcon.setOnClickListener(new View.OnClickListener() {
