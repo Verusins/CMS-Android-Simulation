7dd5ef234e6b274c19fe1159718cbdd062e28f8b
Verusins <115052147+Verusins@users.noreply.github.com>
2023-11-28 13:49:55 -0500

    Finished Admin Dashboard
    Added announcement adapter, modified navbar from S to Admin, added navbar item "Create Events"

--- app/src/main/java/example/com/cmsandroidsimulation/DashboardAdminFragment.java
+++ app/src/main/java/example/com/cmsandroidsimulation/DashboardAdminFragment.java
@@ -4,14 +4,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 import example.com.cmsandroidsimulation.databinding.FragmentDashboardAdminBinding;
+import example.com.cmsandroidsimulation.databinding.FragmentDashboardStudentBinding;
+import example.com.cmsandroidsimulation.models.Announcement;
+import example.com.cmsandroidsimulation.models.EventInfo;
+import example.com.cmsandroidsimulation.models.PlaceholderValues;
 
 public class DashboardAdminFragment extends Fragment {
 
     FragmentDashboardAdminBinding binding;
+    EventAdapter eventAdapter;
+    AnnouncementAdapter announcementAdapter;
+    ArrayList<Announcement> announcementList = PlaceholderValues.generateTestAnnouncementList();
+    ArrayList<EventInfo> eventList = PlaceholderValues.generateTestEventInfoList();
 
     @Override
     public View onCreateView(
@@ -20,5 +35,29 @@ public class DashboardAdminFragment extends Fragment {
     ) {
         binding = FragmentDashboardAdminBinding.inflate(inflater, container, false);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+//        RelativeLayout sidebar = binding.sidebarWrapper;
+
+//        Disable Sidebar on create
+//        sidebar.setVisibility(View.GONE);
+
+//        Announcement from db
+        RecyclerView recyclerViewAnnouncement = binding.RecyclerViewAnnouncement;
+        announcementAdapter = new AnnouncementAdapter(announcementList, getContext());
+        recyclerViewAnnouncement.setAdapter(announcementAdapter);
+        recyclerViewAnnouncement.setLayoutManager(new LinearLayoutManager(getContext()));
+
+//        Event from db
+        RecyclerView recyclerViewEvent = binding.RecyclerViewEvent;
+        eventAdapter = new EventAdapter(eventList, getContext());
+        recyclerViewEvent.setAdapter(eventAdapter);
+        recyclerViewEvent.setLayoutManager(new LinearLayoutManager(getContext()));
+
+
     }
 }
