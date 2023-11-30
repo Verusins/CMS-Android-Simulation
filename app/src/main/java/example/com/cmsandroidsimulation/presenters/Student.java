f145184edebd96c768fb8f589eb500056cd477a3
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2023-11-30 12:57:15 -0500

    Make so app no crash if login as student fails

--- app/src/main/java/example/com/cmsandroidsimulation/presenters/Student.java
+++ app/src/main/java/example/com/cmsandroidsimulation/presenters/Student.java
@@ -44,7 +44,7 @@ public class Student extends User {
                     user = mAuth.getCurrentUser();
                 } else {
                     // If sign in fails, display a message to the user.
-                    throw new FailedLoginException();
+                    Log.e("MASTER APP", "Login failed");
                 }
             }
         });
